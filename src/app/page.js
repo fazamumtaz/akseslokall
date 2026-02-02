627520a5e65eed261267c387f3e23f00db87e3ca
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-02-02 20:35:38 +0700

    fix render

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,41 @@
-"use client";
-import dynamic from "next/dynamic";
-import SidebarPlace from "@/components/Map/SidebarPlace";
-import { Activity, useState } from "react";
+// "use client";
+// import dynamic from "next/dynamic";
+// import SidebarPlace from "@/components/Map/SidebarPlace";
+// import { Activity, useState } from "react";
 
-// 🔎 Panggil wrapper map-nya di sini
+// // 🔎 Panggil wrapper map-nya di sini
+// const MapComponent = dynamic(() => import("@/components/Map/MapComponent"), {
+//   ssr: false,
+//   loading: () => (
+//     <p className="h-full w-full flex items-center justify-center">
+//       Loading Map... ⏳
+//     </p>
+//   ),
+// });
+
+// export default function Home() {
+//   const [sidebarVisible, setSidebarVisible] = useState(false);
+//   return (
+//     <main className="relative w-full h-screen overflow-hidden">
+//       <div className="absolute inset-0 z-0">
+//         <MapComponent />
+//       </div>
+//       <SidebarPlace />
+//       <div className="h-10 w-64 border bg-white rounded shadow-2xl absolute top-5 right-5"></div>
+//     </main>
+//   );
+// }
+
+"use client";
+
+// import MapComponent from "@/components/MapComponent";
+// import Map from "@/components/map";
+import dynamic from "next/dynamic";
+
 const MapComponent = dynamic(() => import("@/components/Map/MapComponent"), {
   ssr: false,
-  loading: () => (
-    <p className="h-full w-full flex items-center justify-center">
-      Loading Map... ⏳
-    </p>
-  ),
 });
 
-export default function Home() {
-  const [sidebarVisible, setSidebarVisible] = useState(false);
-  return (
-    <main className="relative w-full h-screen overflow-hidden">
-      <div className="absolute inset-0 z-0">
-        <MapComponent />
-      </div>
-      <SidebarPlace />
-      <div className="h-10 w-64 border bg-white rounded shadow-2xl absolute top-5 right-5"></div>
-    </main>
-  );
+export default function Page() {
+  return <MapComponent />;
 }
--- src/components/Map/MapComponent.jsx
+++ src/components/Map/MapComponent.jsx
@@ -8,20 +8,27 @@ const position = [-6.1702, 106.8314];
 
 export default function MapComponent() {
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={true}
-      className="h-full w-full"
-    >
-      <TileLayer
-        url="https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token=sQz1rPefoLTumwHeCfgtoK66dV7TyL4pQtCNbJ0GxkdeRFuMIEfr1dvsSPnI5WGl"
-        attribution="..."
-      />
+    <main className="w-full h-screen relative">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="h-full w-full"
+      >
+        {/* <TileLayer
+          url="https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token=sQz1rPefoLTumwHeCfgtoK66dV7TyL4pQtCNbJ0GxkdeRFuMIEfr1dvsSPnI5WGl"
+          attribution="..."
+        /> */}
 
-      {LIST_LOCATION.map((place, index) => (
-        <MapMarker key={place.id || index} place={place} />
-      ))}
-    </MapContainer>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {LIST_LOCATION.map((place, index) => (
+          <MapMarker key={place.id || index} place={place} />
+        ))}
+      </MapContainer>
+    </main>
   );
 }
